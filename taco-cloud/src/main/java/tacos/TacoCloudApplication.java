fef7d188b9b459e0b10923e7a0ac402c0408478e
v.platonov <v.platonov@PLATONOV.yar-krista.ad>
2019-02-04 17:50:50 +0300

    first commit

---
+++ taco-cloud/src/main/java/tacos/TacoCloudApplication.java
@@ -0,0 +1,14 @@
+package tacos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+//Hello my friend
+@SpringBootApplication
+public class TacoCloudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TacoCloudApplication.class, args);
+	}
+
+}
+
