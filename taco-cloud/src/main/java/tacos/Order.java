deed3e4b20e343e6d9b619ece7bfc3376ea5d06a
platonovv <vasilii_yar@mail.ru>
2019-03-12 21:54:34 +0200

    Ch3-change properties

--- taco-cloud/src/main/java/tacos/Order.java
+++ taco-cloud/src/main/java/tacos/Order.java
@@ -1,5 +1,7 @@
 package tacos;
 
+import java.util.Date;
+
 import javax.validation.constraints.Digits;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Pattern;
@@ -10,6 +12,8 @@ import lombok.Data;
 
 @Data
 public class Order {
+	private long id;
+	private Date createAt;
 	@NotBlank(message="Name is required")
     private String name;
 	@NotBlank(message="street is required")
--- taco-cloud/src/main/java/tacos/Taco.java
+++ taco-cloud/src/main/java/tacos/Taco.java
@@ -1,5 +1,7 @@
 package tacos;
 
+
+import java.util.Date;
 import java.util.List;
 
 import javax.validation.constraints.NotNull;
@@ -9,10 +11,13 @@ import lombok.Data;
 
 @Data
 public class Taco {
+	private long id;
 	@NotNull
 	@Size(min=5, message="Name must be at least 5 characters long")
 	private String name;
 	
 	@Size(min=1, message="You mast choose at least 1 ingredients")
 	private List<String> ingredients;
+	
+	private Date createAt;
 }
